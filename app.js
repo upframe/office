37c58a8691a9a3ef2ec58362ffd1fa379dfdf70f
Ulisses Ferreira <ulissesvf@gmail.com>
2017-12-14 01:01:38 +0000

    Vejam o erro

--- app.js
+++ app.js
@@ -2,38 +2,44 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 var fs = require('fs')
-
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var password = process.argv[2]
+
+function userIsLoggedIn (user) {
+  return fs.readFileSync('users.txt').toString().split("\n").includes(user)
+}
+
+function usersFileExists () {
+  return fs.existsSync('users.txt')
+}
 
 app.set('view engine', 'ejs')
 app.use('/css', express.static('css'))
 
 app.get('/', function (req, res) {
-  res.render('welcome')
-})
+  res.render('welcome', { nickTried: '', passTried: '', message: '' })
+});
 
 app.post('/', urlencodedParser, function (req, res) {
-  // temos em req.body.password a password do POST
-  // temos em req.body.nickname o nickname do utilizador
-
-  // temos que verificar que a password está correta
-  // vamos buscar ao ficheiro a password e apagamos espaços
-  var passwordText = fs.readFileSync('thisisnotthepassword.txt', 'utf8').replace(/\s+/g, '')
-  // com o body parser temos acesso ao POST na parte do body com o nome respetivo
-  var passwordSubmitted = req.body.password
-
-  if (passwordText === passwordSubmitted) {
-      // password is correct
-      //
-      // temos que mostrar a pagina de sucesso
-      // acrescentar o utilizador à lista daqueles que aqui estão tambem
-      // var team = JSON.parse(fs.readFileSync('team.json', req.body.nickname));
-      // fs.appendFileSync('team.json', req.body.nickname);
-
-    res.render('office', { nickname: req.body.nickname, users: ['ulisse', 'li'] })
+  var utilizadoresOnline = []
+  if ( password === req.body.password ) {
+    if ( !usersFileExists() ) {
+      fs.writeFileSync('users.txt', '')
+    }
+    if ( userIsLoggedIn(req.body.nickname) ) {
+      //nao precisamos por nada em disco
+      utilizadoresOnline = fs.readFileSync('users.txt').toString().split("\n")
+    } else {
+      //precisamos de adicionar o user a lista
+      utilizadoresOnline = fs.readFileSync('users.txt').toString().split("\n").push(req.body.nickname)
+      fs.unlinkSync('users.txt')
+      for (var i = 0, len = utilizadoresOnline.length; i < len; i++) {
+        fs.appendFileSync('users.txt', utilizadoresOnline[i])
+      }
+    }
+    res.render('office', { nickname: req.body.nickname, users: utilizadoresOnline })
   } else {
-      // password is wrong :(
-    res.render('welcome')
+    res.render('welcome', { nickTried: req.body.nickname, passTried: req.body.password, message: 'Wrong password' })
   }
 })
 
