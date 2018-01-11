151885ba956088d29791d32a0e8039f37662cd1e
Ulisses Ferreira <ulissesvf@gmail.com>
2018-01-11 22:59:47 +0000

    revert: last commit

--- src/index.js
+++ src/index.js
@@ -6,27 +6,4 @@ import './config'
 
 import App from './App'
 
-// experimental w(°ｏ°)w
-var express = require ('express');
-var bodyParser = require ('body-parser');
-var postListener = express();
-
-var Slack = require('slack-node');
-// /exprimental w(°ｏ°)w
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-postListener.post('/getUsers', urlencodedParser, function(req, res){
-  webhookUri = "https://hooks.slack.com/services/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";
-
-  slack = new Slack();
-  slack.setWebhook(webhookUri);
-
-  slack.webhook({
-    channel: "#whosthere",
-    username: "webhookbot",
-    text: "This is posted to #general and comes from a bot named webhookbot."
-  }, function(err, response) {
-    //console.log(response);
-  });
-});
